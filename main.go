a582174d89a580d2fbbaa3a8890f5ae6ddd5ceea
Błażej Mcszk <dzikun@gmail.com>
2023-03-20 20:02:37 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import socks5 "github.com/armon/go-socks5"
+
+func main() {
+
+	conf := &socks5.Config{}
+	server, err := socks5.New(conf)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := server.ListenAndServe("tcp", ":22"); err != nil {
+		panic(err)
+	}
+
+}
