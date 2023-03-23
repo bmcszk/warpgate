562748a5f5f9cbd34e63fc12c49686f1a183aa94
Błażej Mcszk <dzikun@gmail.com>
2023-03-23 19:29:04 +0200

    feat: introduced viper configuration

--- main.go
+++ main.go
@@ -1,16 +1,42 @@
 package main
 
-import socks5 "github.com/armon/go-socks5"
+import (
+	socks5 "github.com/armon/go-socks5"
+	"github.com/spf13/viper"
+)
+
+type WarpgateConfig struct {
+	Serve  ServeConfig
+	Socks5 socks5.Config
+}
+
+type ServeConfig struct {
+	Network string
+	Address string
+}
 
 func main() {
 
-	conf := &socks5.Config{}
-	server, err := socks5.New(conf)
+	viper.SetConfigName("warpgate")
+	viper.AddConfigPath("$HOME/.config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
 	if err != nil {
 		panic(err)
 	}
 
-	if err := server.ListenAndServe("tcp", ":22"); err != nil {
+	var config WarpgateConfig
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		panic(err)
+	}
+
+	server, err := socks5.New(&config.Socks5)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := server.ListenAndServe(config.Serve.Network, config.Serve.Address); err != nil {
 		panic(err)
 	}
 
